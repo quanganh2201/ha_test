ca025d61485a37ec4cc47dbd3a2ac0ef9609e44f
vuweed <halucminhvu2000@gmail.com>
2023-07-11 16:03:41 +0700

    clean code

--- Core/Src/main_process.c
+++ Core/Src/main_process.c
@@ -16,7 +16,6 @@ extern uint8_t axis2_chk;
 extern uint8_t axis3_chk;
 extern uint8_t axis4_chk;
 S_PROCESS sProcess;
-uint8_t RotateOK;
 ret_val_t home_flag = 0;
 static bool SendBLDCTimeout(uint16_t Timeout, FlagStatus1 eStatus)
 {
@@ -29,7 +28,7 @@ static bool SendBLDCTimeout(uint16_t Timeout, FlagStatus1 eStatus)
 	}
 	else
 	{
-		if((HAL_GetTick() - oldTime) >= Timeout)
+		if ((HAL_GetTick() - oldTime) >= Timeout)
 		{
 			flagFinishTask = true;
 		}
@@ -39,7 +38,7 @@ static bool SendBLDCTimeout(uint16_t Timeout, FlagStatus1 eStatus)
 extern uint32_t pre;
 void main_process()
 {
-	switch(sProcess.process)
+	switch (sProcess.process)
 	{
 	case UARThandler:
 		UART2_Handler();
@@ -51,11 +50,11 @@ void main_process()
 			sProcess.frameFront[0] = 0xABCD;
 			sProcess.frameFront[1] = sModule2Params.speed;
 			sProcess.frameFront[2] = sModule1Params.speed;
-			sProcess.frameFront[3] = (uint16_t) (sProcess.frameFront[0] ^sProcess.frameFront[1]^sProcess.frameFront[2]);
+			sProcess.frameFront[3] = (uint16_t)(sProcess.frameFront[0] ^ sProcess.frameFront[1] ^ sProcess.frameFront[2]);
 			sProcess.frameRear[0] = 0xABCD;
 			sProcess.frameRear[1] = -sModule4Params.speed;
 			sProcess.frameRear[2] = -sModule3Params.speed;
-			sProcess.frameRear[3] = (uint16_t) (sProcess.frameRear[0] ^sProcess.frameRear[1]^sProcess.frameRear[2]);
+			sProcess.frameRear[3] = (uint16_t)(sProcess.frameRear[0] ^ sProcess.frameRear[1] ^ sProcess.frameRear[2]);
 			HAL_UART_Transmit(&huart2, (uint8_t *)sProcess.frameFront, sizeof(sProcess.frameFront), 10);
 			HAL_UART_Transmit(&huart6, (uint8_t *)sProcess.frameRear, sizeof(sProcess.frameRear), 10);
 			SendBLDCTimeout(100, RESET1);
@@ -63,92 +62,23 @@ void main_process()
 		sProcess.process++;
 		break;
 	case adjustAngle:
-//		if(RotateOK)// CHO HAM QUAY HUONG VAO DAY
-//		{
-//
-//		}
-//	    if(0 == sModule1Params.targetAngle)
-//	        sModule1Params.targetAngle = 1;
-	    axis1.angle = sModule1Params.targetAngle;
-//        if(0 == sModule2Params.targetAngle)
-//            sModule2Params.targetAngle = 1;
-        axis2.angle = sModule2Params.targetAngle;
-//        if(0 == sModule3Params.targetAngle)
-//            sModule3Params.targetAngle = 1;
-        axis3.angle = sModule3Params.targetAngle;
-//        if(0 == sModule4Params.targetAngle)
-//            sModule4Params.targetAngle = 1;
-        axis4.angle = sModule4Params.targetAngle;
 
-//        if(sVehicalParams.Vy != 0 && sVehicalParams.Vx == 0 && sVehicalParams.W == 0 \
-//                && axis1_chk == 1 && axis2_chk == 1 && axis3_chk == 1 && axis4_chk == 1)
-//        {
-//            axis1_chk = 0;
-//            axis2_chk = 0;
-//            axis3_chk = 0;
-//            axis4_chk = 0;
-//        }
-         if(sVehicalParams.Vy != 0 && sVehicalParams.Vx == 0 && sVehicalParams.W == 0 \
-                && home_flag != SUCCESSFUL)
-         {
-             home_flag = auto_home();
-//              __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, 0);
-//              __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, HOME_SPEED);
-//
-//              __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_3, 0);
-//              __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_4, HOME_SPEED);
-//
-//              __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, 0);
-//              __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_2, HOME_SPEED);
-//
-//              __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 0);
-//              __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_4, HOME_SPEED);
-//
-//
-//              while((axis1_chk != 1) || (axis2_chk != 1) || (axis3_chk != 1) || (axis4_chk != 1))
-//              {
-//                  /*Check each motor if reaching end stop*/
-//                  if(HAL_GPIO_ReadPin (GPIOA, GPIO_PIN_15) == 1)
-//                  {
-//                      __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 0);
-//                      axis1_chk = 1;
-//                  }
-//                  if(HAL_GPIO_ReadPin (GPIOB, GPIO_PIN_3) == 1)
-//                  {
-//                      __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_4, 0);
-//                      axis2_chk = 1;
-//                  }
-//                  if(HAL_GPIO_ReadPin (GPIOB, GPIO_PIN_4) == 1)
-//                  {
-//                      __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_2, 0);
-//                      axis3_chk = 1;
-//
-//                  }
-//                  if(HAL_GPIO_ReadPin (GPIOB, GPIO_PIN_5) == 1)
-//                  {
-//                      __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_4, 0);
-//                      axis4_chk = 1;
-//                  }
+		axis1.angle = sModule1Params.targetAngle;
 
-         }
-                    pwm_handler(&htim3, &axis1, cnt1, CH1_CH2);
-                    pwm_handler(&htim3, &axis2, cnt2, CH3_CH4);
-                    pwm_handler(&htim4, &axis3, cnt3, CH1_CH2);
-                    pwm_handler(&htim4, &axis4, cnt4, CH3_CH4);
-//        if ( HAL_GetTick() - pre <= 10000)
-//        {
-//            pwm_handler(&htim3, &axis1, cnt1, CH1_CH2);
-//            pwm_handler(&htim3, &axis2, cnt2, CH3_CH4);
-//            pwm_handler(&htim4, &axis3, cnt3, CH1_CH2);
-//            pwm_handler(&htim4, &axis4, cnt4, CH3_CH4);
-//        }
-//        else
-//        {
-//            axis1.pwm = 0;
-//            axis2.pwm = 0;
-//            axis3.pwm = 0;
-//            axis4.pwm = 0;
-//        }
+		axis2.angle = sModule2Params.targetAngle;
+
+		axis3.angle = sModule3Params.targetAngle;
+
+		axis4.angle = sModule4Params.targetAngle;
+		/*Re calib axis angle, run till reaching end stop*/
+		if (sVehicalParams.Vy != 0 && sVehicalParams.Vx == 0 && sVehicalParams.W == 0 && home_flag != SUCCESSFUL)
+		{
+			home_flag = auto_home();
+		}
+		pwm_handler(&htim3, &axis1, cnt1, CH1_CH2);
+		pwm_handler(&htim3, &axis2, cnt2, CH3_CH4);
+		pwm_handler(&htim4, &axis3, cnt3, CH1_CH2);
+		pwm_handler(&htim4, &axis4, cnt4, CH3_CH4);
 
 		sProcess.process = UARThandler;
 		break;
@@ -157,4 +87,3 @@ void main_process()
 		break;
 	}
 }
-
