15beaf6d2c8e2fbe0f84ceefa3b39cee0cd7e295
vuweed <halucminhvu2000@gmail.com>
2023-07-11 16:00:27 +0700

    format code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,27 +1,27 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include"DATN_Comm.h"
+#include "DATN_Comm.h"
 #include "Swerve_Kinematics.h"
 #include "axis_driver.h"
 #include "main_process.h"
@@ -55,11 +55,12 @@ UART_HandleTypeDef huart6;
 volatile uint32_t cnt1 = HOME_OFFSET, cnt2 = HOME_OFFSET, cnt3 = HOME_OFFSET, cnt4 = HOME_OFFSET;
 uint32_t DT = 0;
 uint32_t previousTime;
-uint32_t curTime = 0;;
-M_axis_t axis1 = {0,90};
-M_axis_t axis2 = {0,90};
-M_axis_t axis3 = {0,90};
-M_axis_t axis4 = {0,90};
+uint32_t curTime = 0;
+;
+M_axis_t axis1 = {0, 90};
+M_axis_t axis2 = {0, 90};
+M_axis_t axis3 = {0, 90};
+M_axis_t axis4 = {0, 90};
 uint8_t TEST;
 
 /* USER CODE END PV */
@@ -82,62 +83,62 @@ static void MX_TIM2_Init(void);
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 {
+  /*Axis 1: channel 1, pin A8*/
+  if (htim->Instance == htim2.Instance)
+  {
     /*Axis 1: channel 1, pin A8*/
-    if (htim->Instance == htim2.Instance)
+    axis1.dir = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8);
+    if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
     {
-        /*Axis 1: channel 1, pin A8*/
-        axis1.dir = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8);
-        if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
-        {
-             if(!axis1.dir)
-                 cnt1++;
-             else
-             {
-                 /*Avoiding floating value*/
-                 cnt1 = (cnt1 > MIN_AXIS_VAL) ? (cnt1 - 1) : 0;
-             }
-        }
-        axis2.dir = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_15);
-        if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2)
-        {
-             if(!axis2.dir)
-                 cnt2++;
-             else
-             {
-                 cnt2 = (cnt2 > MIN_AXIS_VAL) ? (cnt2 - 1) : 0;
-             }
-        }
-        axis3.dir = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14);
-        if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_3)
-        {
-             if(!axis3.dir)
-                 cnt3++;
-             else
-             {
-                 cnt3 = (cnt3 > MIN_AXIS_VAL) ? (cnt3 - 1) : 0;
-             }
-        }
+      if (!axis1.dir)
+        cnt1++;
+      else
+      {
+        /*Avoiding floating value*/
+        cnt1 = (cnt1 > MIN_AXIS_VAL) ? (cnt1 - 1) : 0;
+      }
     }
-    if (htim->Instance == htim5.Instance)
+    axis2.dir = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_15);
+    if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2)
     {
-        axis4.dir = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13);
-        if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
-        {
-             if(!axis4.dir)
-                 cnt4++;
-             else
-             {
-                 cnt4 = (cnt4 > MIN_AXIS_VAL) ? (cnt4 - 1) : 0;
-             }
-        }
+      if (!axis2.dir)
+        cnt2++;
+      else
+      {
+        cnt2 = (cnt2 > MIN_AXIS_VAL) ? (cnt2 - 1) : 0;
+      }
     }
+    axis3.dir = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14);
+    if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_3)
+    {
+      if (!axis3.dir)
+        cnt3++;
+      else
+      {
+        cnt3 = (cnt3 > MIN_AXIS_VAL) ? (cnt3 - 1) : 0;
+      }
+    }
+  }
+  if (htim->Instance == htim5.Instance)
+  {
+    axis4.dir = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13);
+    if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
+    {
+      if (!axis4.dir)
+        cnt4++;
+      else
+      {
+        cnt4 = (cnt4 > MIN_AXIS_VAL) ? (cnt4 - 1) : 0;
+      }
+    }
+  }
 }
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -148,8 +149,7 @@ int main(void)
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 
-
-    HAL_Init();
+  HAL_Init();
 
   /* USER CODE BEGIN Init */
 
@@ -179,35 +179,32 @@ int main(void)
   HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_3);
   HAL_TIM_IC_Start_IT(&htim5, TIM_CHANNEL_1);
   /*Start pwm*/
-  HAL_TIM_PWM_Start (&htim3, TIM_CHANNEL_1);
-  HAL_TIM_PWM_Start (&htim3, TIM_CHANNEL_2);
-  HAL_TIM_PWM_Start (&htim3, TIM_CHANNEL_3);
-  HAL_TIM_PWM_Start (&htim3, TIM_CHANNEL_4);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
 
-  HAL_TIM_PWM_Start (&htim4, TIM_CHANNEL_1);
-  HAL_TIM_PWM_Start (&htim4, TIM_CHANNEL_2);
-  HAL_TIM_PWM_Start (&htim4, TIM_CHANNEL_3);
-  HAL_TIM_PWM_Start (&htim4, TIM_CHANNEL_4);
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_4);
   Init_UART_Recv();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   auto_home();
-
   /*Blynk led to notice*/
-
-    for(i = 0; i < 10; i++)
-    {
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, RESET);
-        HAL_Delay(150);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, SET);
-        HAL_Delay(150);
-    }
-//  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, RESET);
+  for (i = 0; i < 10; i++)
+  {
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, RESET);
+    HAL_Delay(150);
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, SET);
+    HAL_Delay(150);
+  }
   while (1)
   {
-      main_process();
+    main_process();
 
     /* USER CODE END WHILE */
 
@@ -217,22 +214,22 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
@@ -242,9 +239,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -257,10 +253,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief TIM2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM2_Init(void)
 {
 
@@ -275,7 +271,7 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 25-1;
+  htim2.Init.Prescaler = 25 - 1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 4294967295;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -309,14 +305,13 @@ static void MX_TIM2_Init(void)
   /* USER CODE BEGIN TIM2_Init 2 */
 
   /* USER CODE END TIM2_Init 2 */
-
 }
 
 /**
-  * @brief TIM3 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM3 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM3_Init(void)
 {
 
@@ -380,14 +375,13 @@ static void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 2 */
   HAL_TIM_MspPostInit(&htim3);
-
 }
 
 /**
-  * @brief TIM4 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM4 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM4_Init(void)
 {
 
@@ -451,14 +445,13 @@ static void MX_TIM4_Init(void)
 
   /* USER CODE END TIM4_Init 2 */
   HAL_TIM_MspPostInit(&htim4);
-
 }
 
 /**
-  * @brief TIM5 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM5 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM5_Init(void)
 {
 
@@ -499,14 +492,13 @@ static void MX_TIM5_Init(void)
   /* USER CODE BEGIN TIM5_Init 2 */
 
   /* USER CODE END TIM5_Init 2 */
-
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART2_UART_Init(void)
 {
 
@@ -532,14 +524,13 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
-  * @brief USART6 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART6 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART6_UART_Init(void)
 {
 
@@ -565,19 +556,18 @@ static void MX_USART6_UART_Init(void)
   /* USER CODE BEGIN USART6_Init 2 */
 
   /* USER CODE END USART6_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -596,7 +586,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : B4_Pin B3_Pin B2_Pin */
-  GPIO_InitStruct.Pin = B4_Pin|B3_Pin|B2_Pin;
+  GPIO_InitStruct.Pin = B4_Pin | B3_Pin | B2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
@@ -614,7 +604,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PB3 PB4 PB5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5;
+  GPIO_InitStruct.Pin = GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
@@ -632,8 +622,8 @@ static void MX_GPIO_Init(void)
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -641,9 +631,9 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -655,14 +645,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
